d898fecceda92632bbf6a3f710f9081fa86dd3e0
Mark D. Butala <butala@illinois.edu>
2016-01-29 17:08:04 -0600

    Added comments.

--- mag/repository.py
+++ mag/repository.py
@@ -7,9 +7,18 @@ from ..util.date import fromJ2000
 TEMPLATE_MAP = {'definitive': '/rdata/airglow/butala/data/intermagnet/{date:%Y}/ams/IAGA2002/{stn}{date:%Y%m%d}d.min',
                 0:            '/rdata/airglow/butala/data/intermagnet_level0/{year}/{stn}.{year}.nc',
                 1:            '/rdata/airglow/butala/data/intermagnet_level1/{year}/{stn}.{year}.nc'}
+"""
+Mapping between INTERMAGNET repository data types and file name
+template.
+"""
 
 
 def nc_to_dataframe(nc_fname):
+    """
+    Return a pandas data frame containing the information in the
+    netCDF file *nc_fname*. Return it and a mapping to the header
+    metadata.
+    """
     root = Dataset(nc_fname)
     data = {}
     data.update({dim: root[dim][:] for dim in root.dimensions if dim != 'time'})
