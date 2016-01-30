0729aeb08ecf904850d94b19e08d4dc70a9a6199
Mark D. Butala <butala@illinois.edu>
2016-01-30 11:26:51 -0600

    Added column slice capability.

--- mag/repository.py
+++ mag/repository.py
@@ -18,17 +18,19 @@ template.
 """
 
 
-def nc_to_dataframe(nc_fname):
+def nc_to_dataframe(nc_fname,
+                    columns=slice(None)):
     """
     Return a pandas data frame containing the information in the
     netCDF file *nc_fname*. Return it and a mapping to the header
-    metadata.
+    metadata. Use *columns* to select columns (via a list of column
+    names).
     """
     root = Dataset(nc_fname)
     data = {}
     data.update({dim: root[dim][:] for dim in root.dimensions if dim != 'time'})
     index = data['time'] = map(fromJ2000, root['time'][:])
-    return (PD.DataFrame(data=data, index=index),
+    return (PD.DataFrame(data=data, index=index)[columns],
             {x: getattr(root, x) for x in root.ncattrs()})
 
 
@@ -46,18 +48,21 @@ def repository_years(level):
     return sorted(year_list)
 
 
-def complete_time_series(stn, level):
+def complete_time_series(stn,
+                         level,
+                         columns=slice(None)):
     """
     Return a pandas data frame and mapping header information (one
     entry per year) for all available data for a given *stn* and data
-    product *level*.
+    product *level*. Use *columns* to select columns (via a list of
+    column names).
     """
     frames = []
     info_map = {}
     for year in repository_years(level):
         nc_fname = TEMPLATE_MAP[level].format(year=year, stn=stn)
         logger.info('loading {}'.format(nc_fname))
-        dataframe_i, info_i = nc_to_dataframe(nc_fname)
+        dataframe_i, info_i = nc_to_dataframe(nc_fname, columns=columns)
         frames.append(dataframe_i)
         info_map[year] = info_i
     return (PD.concat(frames),
@@ -67,5 +72,5 @@ def complete_time_series(stn, level):
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     # combine all netCDF records for a given station
-    dataframe, info_map = complete_time_series('frd', 0)
+    dataframe, info_map = complete_time_series('frd', 0, columns=['time', 'H'])
     dataframe.info()
