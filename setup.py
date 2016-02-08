be303fcbc37ff100687821672409820ea7555cc6
Mark D. Butala <butala@illinois.edu>
2016-02-08 12:47:28 -0600

    Added Matt to author list.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(name='pyrsss',
       version='0.1',
       description='Remote sensing and space science python tools.',
-      author='Mark D. Butala, et al.',
+      author='Mark D. Butala, Matthew A. Grawe, et al.',
       author_email='butala@illinois.edu',
       package_dir = {'pyrsss': '.'},
       packages=['pyrsss',
