2cc663be2f6ff9476e93b56d0890e2fff7607dc8
Mark D. Butala <butala@illinois.edu>
2016-02-18 16:11:08 -0600

    Now return Series instead of DataFrame.

--- mag/usgs_dst.py
+++ mag/usgs_dst.py
@@ -10,10 +10,16 @@ from repository import get_root
 def date_parser(*cols):
     def get_int(x):
         return int(x[:-10])
-    return [datetime(*map(get_int, [year, month, day])) + timedelta(minutes=get_int(minute) - 1) for month, day, year, minute in izip(*cols)]
+    return [datetime(*map(get_int, [year, month, day])) + \
+            timedelta(minutes=get_int(minute) - 1) for (month,
+                                                        day,
+                                                        year,
+                                                        minute) in izip(*cols)]
 
 
-def read_dst(fname=os.path.join(get_root(), 'USGS_dst', 'Dst_definitive_fix_minute.out')):
+def read_dst(fname=os.path.join(get_root(),
+                                'USGS_dst',
+                                'Dst_definitive_fix_minute.out')):
     df = PD.read_csv(fname,
                      header=None,
                      names=['month', 'day', 'year', 'minute', 'doy', 'dst'],
@@ -23,4 +29,5 @@ def read_dst(fname=os.path.join(get_root(), 'USGS_dst', 'Dst_definitive_fix_minu
                      usecols=['month', 'day', 'year', 'minute', 'dst'],
                      date_parser=date_parser)
     df.columns = ['dt', 'dst']
-    return df
+    return PD.Series(data=usgs_dst['dst'].values,
+                     index=usgs_dst['dt'].values)
