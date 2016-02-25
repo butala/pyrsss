8ca4f96f12b0f321b6dfc06622278a8d39cc8245
Mark D. Butala <butala@illinois.edu>
2016-02-25 08:37:33 -0600

    Added some comments.

--- emission/v6300.py
+++ emission/v6300.py
@@ -15,40 +15,69 @@ from enum import Enum
 OplusType = Enum('O2type', 'ne charge_neutrality')
 
 
+"""
+The following (alpha1 through BETA_1D) are from Table 2.2 from
+Makela's dissertation. They are in turn from:
+
+Link and Cogger, A reexamination of the O I 6300 \AA nightglow,
+Journal of Geophysical Research, vol. 93, pp. 9883--9892, 1988.
+"""
 
 def alpha1(Te):
-    return 1.95e-7 * (Te / 300)**(-0.7)
+    """[cm^3 / s]"""
+    return 1.95e-7 * (Te / 300.)**(-0.7)
+
 
 def alpha2(Te):
-    return 4.00e-7 * (Te / 300)**(-0.9)
+    """[cm^3 / s]"""
+    return 4.00e-7 * (Te / 300.)**(-0.9)
+
 
 def k1(Ti):
+    """[cm^3 / s]"""
     return 3.23e-12 * exp(3.72/(Ti/300) - 1.87/(Ti/300)**2)
 
+
 def k2(Ti):
+    """[cm^3 / s]"""
     return 2.78e-13 * exp(2.07/(Ti/300) - 0.61/(Ti/300)**2)
 
+
 def k3(Tn):
+    """[cm^3 / s]"""
     return 2.0e-11 * exp(111.8/Tn)
 
+
 def k4(Tn):
+    """[cm^3 / s]"""
     return 2.9e-11 * exp(67.5/Tn)
 
+
 def k5(Tn):
+    """[cm^3 / s]"""
     return 1.6e-12 * Tn**(0.91)
 
+
 A_1D = 6.81e-3
+"""[1/s]"""
+
 
 BETA_1D = 1.1
 
+
 def Oplus_simple(ne):
+    """
+    """
     return ne
+
 
 def Oplus(ne,
           Te,
           Ti,
           O2,
           N2):
+    """
+    """
     return ne / (1 \
                  + k1(Ti) * O2 / (alpha1(Te) * ne) \
                  + k2(Ti) * N2 / (alpha2(Te) * ne))
@@ -60,6 +89,8 @@ def emission(ne,
              O2,
              N2,
              oplus_type=OplusType.charge_neutrality):
+    """
+    """
     if oplus_type == OplusType.ne:
         oplus = Oplus_simple(ne)
     elif oplus_type == OplusType.charge_neutrality:
