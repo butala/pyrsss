32d514b88ccbdb2822fe6673ea69194df0c94a29
Mark D. Butala <butala@illinois.edu>
2016-02-25 15:39:18 -0600

    Removed all sympy and test code.
    It was causing interference with numpy in other codes.

--- emission/v6300.py
+++ emission/v6300.py
@@ -2,10 +2,6 @@ from __future__ import division
 
 from datetime import datetime
 
-import sympy as SP
-from sympy import exp
-import numpy as NP
-import pylab as PL
 from pyglow.pyglow import Point
 # using back-port for python2 (enum34 package) --- this is in the
 # python3 standard library (>=3.4)
@@ -100,72 +96,3 @@ def emission_v6300(ne,
     N = 0.76 * BETA_1D * k1(Ti) * O2 * oplus
     D = 1 + (k3(Tn) * N2 + k4(Tn) * O2 + k5(Tn) * ne) / A_1D
     return N / D
-
-
-if __name__ == '__main__':
-    ne, Te, Ti, Tn, O2, N2 = SP.symbols('ne Te Ti Tn O2 N2')
-    v6300 = emission_6300(ne, Te, Ti, Tn, O2, N2)
-    v6300_f = SP.lambdify((ne, Te, Ti, Tn, O2, N2),
-                          v6300)
-
-    d_v6300_ne = SP.simplify(SP.diff(v6300, ne))
-    d_v6300_ne_f = SP.lambdify((ne, Te, Ti, Tn, O2, N2),
-                               d_v6300_ne)
-
-    dn = datetime(1999, 3, 13)
-    lat = 25
-    lon = 0
-    alts = NP.linspace(100, 1000, 256)
-
-    v6300_ne = []
-    v6300_neutrality = []
-    d_v6300_neutrality = []
-    for alt in alts:
-        pt = Point(dn, lat, lon, alt)
-        pt.run_msis()
-        pt.run_iri()
-        v6300_ne.append(emission_6300(pt.ne,
-                                      pt.Te,
-                                      pt.Ti,
-                                      pt.Tn_msis,
-                                      pt.nn['O2'],
-                                      pt.nn['N2'],
-                                      oplus_type=OplusType.ne))
-        v6300_neutrality.append(v6300_f(pt.ne,
-                                        pt.Te,
-                                        pt.Ti,
-                                        pt.Tn_msis,
-                                        pt.nn['O2'],
-                                        pt.nn['N2']))
-        d_v6300_neutrality.append(d_v6300_ne_f(pt.ne,
-                                               pt.Te,
-                                               pt.Ti,
-                                               pt.Tn_msis,
-                                               pt.nn['O2'],
-                                               pt.nn['N2']))
-
-
-    PL.close('all')
-
-    fig = PL.figure(figsize=(8.5, 11))
-    PL.plot(v6300_ne,
-            alts,
-            color='b',
-            label='[O$^+$] = [e]')
-    PL.plot(v6300_neutrality,
-            alts,
-            color='r',
-            label='[e] = [O$^+$] + [$\mathrm{O}_2^+$] + [NO$^+$]')
-    PL.xlabel('V$_{6300}$ [ph/cm$^3$/s]')
-    PL.ylabel('Altitude [km]')
-    PL.legend()
-    PL.title('630.0 [nm] Emission vs. Altitude')
-
-    # UNITS?
-    # EXPRESS IN TECU
-    fig = PL.figure(figsize=(8.5, 11))
-    PL.plot(d_v6300_neutrality,
-            alts,
-            color='b')
-
-    PL.show()
