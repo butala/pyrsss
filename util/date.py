4ecf368791084281f1dd29bfd8e9580b3a520b9b
Mark D. Butala <butala@illinois.edu>
2016-02-25 11:14:47 -0600

    Added local time conversion functions.

--- util/date.py
+++ util/date.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 from datetime import datetime, timedelta
 
 
@@ -17,3 +19,19 @@ def fromJ2000(seconds):
     Convert *seconds* to a :class:`datetime` object.
     """
     return J2000_EPOCH + timedelta(seconds=seconds)
+
+
+def ut2lt(dt_ut, lon):
+    """
+    Convert :class:`datetime` expressed in UT *dt_ut* to the local
+    time at longitude *lon* (in [deg]).
+    """
+    return dt_ut + timedelta(hours=lon * 24 / float(360))
+
+
+def lt2ut(dt_lt, lon):
+    """
+    Convert :class:`datetime` expressed in local time *dt_lt* at
+    longitude *lon* (in [deg]) to UT.
+    """
+    return dt_lt - timedelta(hours=lon * 24 / float(360))
