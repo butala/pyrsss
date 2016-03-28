2955aa45cc828f35015f3038ec344cc576f7b0dd
Mark D. Butala <butala@illinois.edu>
2016-03-28 16:38:42 -0500

    Fix reported coordinate system and output entries for missing records.
    A missing record is denoted with all values 88888, as per the IAGA2002
    standard.

--- mag/sm2iaga.py
+++ mag/sm2iaga.py
@@ -3,10 +3,15 @@ import os
 import logging
 import math
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
-from datetime import datetime
-from collections import defaultdict
+from datetime import datetime, timedelta
+from collections import defaultdict, OrderedDict
 
 import pandas as PD
+
+
+"""
+NOTE: This code assumes that the input and output data are 1-minute interval.
+"""
 
 
 HEADER_TEMPLATE = """\
@@ -35,29 +40,43 @@ def get_reported(df):
     """
     """
     if all([x in df.columns for x in ['N', 'E', 'Z']]):
-        return 'XYZF', {'C1': ('X', 'N'),
+        return 'HDZF', {'C1': ('X', 'N'),
                         'C2': ('Y', 'E'),
                         'C3': ('Z', 'Z')}
     raise ValueError('could not determine coordinate system from columns: ' + ', '.join(df.columns))
 
 
-def dataframe2iaga(fname, stn, df, header_template=HEADER_TEMPLATE):
+def dataframe2iaga(fname, stn, date, df, header_template=HEADER_TEMPLATE):
     """
+    ASSUMES DF SPANS ONLY A SINGLE DAY
     """
     reported, coord_map = get_reported(df)
+    # build map of data records
+    data_map = {}
+    for _, row in df.iterrows():
+        C1 = row[coord_map['C1'][1]]
+        C2 = row[coord_map['C2'][1]]
+        C3 = row[coord_map['C3'][1]]
+        F = math.hypot(math.hypot(C1, C2), C3)
+        data_map[row.Date_UTC] = (C1, C2, C3, F)
+    # fill missing records
+    filled_data = []
+    dts = list(PD.date_range(start=date,
+                             end=date + timedelta(days=1),
+                             freq='min'))[:-1]
+    for dt in dts:
+        filled_data.append(data_map.get(dt,
+                                        (88888,) * 4))
+    # write data records to file
     with open(fname, 'w') as fid:
         fid.write(header_template.format(stn=stn,
                                          reported=reported,
                                          C1=coord_map['C1'][0],
                                          C2=coord_map['C2'][0],
                                          C3=coord_map['C3'][0]))
-        for _, row in df.iterrows():
-            C1 = row[coord_map['C1'][1]]
-            C2 = row[coord_map['C2'][1]]
-            C3 = row[coord_map['C3'][1]]
-            F = math.hypot(math.hypot(C1, C2), C3)
+        for dt, (C1, C2, C3, F) in zip(dts, filled_data):
             fid.write('{date:%Y-%m-%d %H:%M:%S.000} {date:%j}'
-                      '    {C1:>9.2f} {C2:>9.2f} {C3:>9.2f} {F:>9.2f}\n'.format(date=row.Date_UTC,
+                      '    {C1:>9.2f} {C2:>9.2f} {C3:>9.2f} {F:>9.2f}\n'.format(date=dt,
                                                                                 C1=C1,
                                                                                 C2=C2,
                                                                                 C3=C3,
@@ -84,7 +103,7 @@ def df_map2iaga(path, df_map):
     for stn, df in df_map.iteritems():
         for date, df_date in df.groupby(df['Date_UTC'].map(lambda x: x.date())):
             output_fname = iaga_fname(path, stn, date)
-            dataframe2iaga(output_fname, stn, df_date)
+            dataframe2iaga(output_fname, stn, date, df_date)
             output_map[stn][date] = output_fname
     return output_map
 
