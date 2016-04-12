16e709c0dc7288dea66623d55b679caf26b79ae4
butala <butala@illinois.edu>
2016-04-12 16:45:26 -0500

    In place modifications no longer supported. Decimation option added.

--- gps/preprocess.py
+++ gps/preprocess.py
@@ -6,50 +6,62 @@ from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 import sh
 
-from .. util.path import SmartTempDir
+from .. util.path import replace_path
 
 logger = logging.getLogger('pyrsss.gps.preprocess')
 
 
-def normalize_rinex(rinex_fname,
+def normalize_rinex(output_rinex_fname,
+                    rinex_fname,
                     gps=True,
                     glonass=False,
                     sbas=False,
                     galileo=False,
                     compass=False,
                     qzss=False,
+                    decimate=None,
                     clockprep=True,
-                    obs_types = 'L1L2P1P2C1',
-                    work_path=None):
+                    obs_types = 'L1L2P1P2C1'):
     """
-    Sanitize *rinex_fname* (warning: in place) with teqc (which is
-    expected to be found in the path). Satellite system data are
-    excluded if *gps*, *glonass*, *sbas*, *galileo*, *compass*, or
-    *qzss* are `False`. If *clockprep*, smooth time tags (useful for
-    receivers with steered clocks). Only output the observations givne
-    in *obs_types*. Store intermediate files in *work_path*. Return
-    *rinex_fname*.
+    Sanitize *rinex_fname* with teqc (which is expected to be found in
+    the path) and store output in *output_rinex_fname*. Satellite
+    system data are excluded if *gps*, *glonass*, *sbas*, *galileo*,
+    *compass*, or *qzss* are `False`. If *decimate* is given, decimate
+    the output to the given interval (in [s]). If *clockprep*, smooth
+    time tags (useful for receivers with steered clocks). Only output
+    the observations givne in *obs_types*. Return
+    *output_rinex_fname*.
     """
-    with SmartTempDir(work_path) as work_path:
-        rinex_copy_fname = os.path.join(work_path, os.path.basename(rinex_fname))
-        logger.info('copying {} to {}'.format(rinex_fname,
-                                              rinex_copy_fname))
-        shutil.copyfile(rinex_fname, rinex_copy_fname)
-        args = ['+C2']
-        args += ['+G'] if gps else ['-G']
-        args += ['+R'] if glonass else ['-R']
-        args += ['+S'] if sbas else ['-S']
-        args += ['+E'] if galileo else ['-E']
-        args += ['+C'] if compass else ['-C']
-        args += ['+J'] if qzss else ['-J']
-        args += ['+smtt'] if clockprep else ['-smtt']
-        args += ['-O.obs_types', obs_types]
-        args += [rinex_copy_fname]
-        logger.info('calling teqc with arguments: {}'.format(' '.join(args)))
-        sh.teqc(args,
-                _out=rinex_fname,
-                _err=sys.stderr)
-    return rinex_fname
+    args = ['+C2']
+    args += ['+G'] if gps else ['-G']
+    args += ['+R'] if glonass else ['-R']
+    args += ['+S'] if sbas else ['-S']
+    args += ['+E'] if galileo else ['-E']
+    args += ['+C'] if compass else ['-C']
+    args += ['+J'] if qzss else ['-J']
+    args += ['+smtt'] if clockprep else ['-smtt']
+    args += ['-O.obs_types', obs_types]
+    if decimate is not None:
+        args += ['-O.dec', str(decimate)]
+    args += [rinex_fname]
+    logger.info('calling teqc with arguments: {}'.format(' '.join(args)))
+    sh.teqc(args,
+            _out=output_rinex_fname,
+            _err=sys.stderr)
+    return output_rinex_fname
+
+
+def normalize_to_path(path,
+                   rinex_fnames,
+                   **kwds):
+    """ ??? """
+    output_rinex_fnames = []
+    for rinex_fname in rinex_fnames:
+        output_rinex_fnames.append(replace_path(path, rinex_fname))
+        normalize_rinex(output_rinex_fnames[-1],
+                        rinex_fname,
+                        **kwds)
+    return output_rinex_fnames
 
 
 def main(argv=None):
@@ -58,6 +70,9 @@ def main(argv=None):
 
     parser = ArgumentParser('Normalize a given RINEX file.',
                             formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument('path',
+                        type=str,
+                        help='output normalized RINEX files to path')
     parser.add_argument('rinex_fnames',
                         nargs='+',
                         type=str,
@@ -72,7 +87,7 @@ def main(argv=None):
                         action='store_true',
                         help='exclude GLONASS measurements')
     parser.add_argument('--sbas',
-                        '-s',
+                       '-s',
                         action='store_true',
                         help='exclude SBAS measurements')
     parser.add_argument('--galileo',
@@ -87,22 +102,22 @@ def main(argv=None):
                         '-j',
                         action='store_true',
                         help='exclude QZSS measurements')
-    parser.add_argument('--work-path',
-                        '-w',
-                        type=str,
+    parser.add_argument('--decimate',
+                        '-d',
+                        type=int,
                         default=None,
-                        help='location to store intermediate files')
+                        help='decimate output to given interval in [s] (and adjust header accordingly)')
     args = parser.parse_args(argv[1:])
 
-    for rinex_fname in args.rinex_fnames:
-        normalize_rinex(rinex_fname,
-                        gps=not args.gps,
-                        glonass=not args.glonass,
-                        sbas=not args.sbas,
-                        galileo=not args.galileo,
-                        compass=not args.compass,
-                        qzss=not args.qzss,
-                        work_path=args.work_path)
+    normalize_to_path(args.path,
+                      args.rinex_fnames,
+                      gps=not args.gps,
+                      glonass=not args.glonass,
+                      sbas=not args.sbas,
+                      galileo=not args.galileo,
+                      compass=not args.compass,
+                      qzss=not args.qzss,
+                      decimate=args.decimate)
 
 
 if __name__ == '__main__':
