ae02b2abbdad194136b6d0dd2d2fa3560af0ae13
butala <butala@illinois.edu>
2016-04-12 16:46:08 -0500

    Avoid intermediate file generation pollution.

--- gps/teqc.py
+++ gps/teqc.py
@@ -1,16 +1,22 @@
 import sys
+import os
 import logging
 
 import sh
+
+from ..util.path import SmartTempDir, replace_path
 
 logger = logging.getLogger('pyrsss.gps.teqc')
 
 
 def rinex_info(rinex_fname,
-               nav_fname):
+               nav_fname,
+               work_path=None):
     """
     Query RINEX file *rinex_fname* and RINEX nav file *nav_fname* for
-    useful information and return in a key/value mapping.
+    useful information and return in a key/value mapping. Store
+    intermediate files in *work_path* (a temporary, automatically
+    cleaned up area if not specified).
     """
     # information mapping
     info = {}
@@ -45,26 +51,27 @@ def rinex_info(rinex_fname,
             info['MP12'] = float(line.split(':')[1].rstrip()[:-1])
         elif line.startswith('Moving average MP21'):
             info['MP21'] = float(line.split(':')[1].rstrip()[:-1])
-    # error handling
-    error = ''
-    def process_error(line):
-        error += line
-    # query the RINEX file via teqc quality check
-    sh.teqc('+qc',
-            '+quiet',
-            '-R',
-            '-S',
-            '-E',
-            '-C',
-            '-J',
-            '-nav', nav_fname,
-            rinex_fname,
-            _out=process_output,
-            _err=process_error)
-    if error:
-        raise RuntimeError('failure running teqc on '
-                           '{} nav={} ({})'.format(rinex_fname,
-                                                   nav_fname))
+    # query the RINEX file via teqc quality check --- process in given
+    # work area to avoid intermediate file pollution
+    with SmartTempDir(work_path) as work_path:
+        intermediate_rinex_fname = replace_path(work_path, rinex_fname)
+        os.symlink(rinex_fname, intermediate_rinex_fname)
+        intermediate_nav_fname = replace_path(work_path, nav_fname)
+        os.symlink(nav_fname, intermediate_nav_fname)
+        sh.teqc('+qc',
+                '+quiet',
+                '-R',
+                '-S',
+                '-E',
+                '-C',
+                '-J',
+                '-nav', intermediate_nav_fname,
+                intermediate_rinex_fname,
+                _cwd=work_path,
+                _out=process_output,
+                _err=sys.stderr)
+        os.remove(intermediate_rinex_fname)
+        os.remove(intermediate_nav_fname)
     return info
 
 
