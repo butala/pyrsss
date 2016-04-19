d5dec765cb939b383a4e59d61bb1040c6295c858
Mark D. Butala <butala@illinois.edu>
2016-04-19 13:34:22 -0500

    Added tail function.

--- util/path.py
+++ util/path.py
@@ -56,3 +56,35 @@ def replace_path(path, fname):
     Return the replacement the path of *fname* with *path*.
     """
     return os.path.join(path, os.path.basename(fname))
+
+
+def tail(f, window=20):
+    """
+    Returns the last *window* lines of file object *f* as a list.
+
+    From http://stackoverflow.com/a/7047765/6226265.
+    """
+    if window == 0:
+        return []
+    BUFSIZ = 1024
+    f.seek(0, 2)
+    bytes_i = f.tell()
+    size = window + 1
+    block = -1
+    data = []
+    while size > 0 and bytes_i > 0:
+        if bytes_i - BUFSIZ > 0:
+            # Seek back one whole BUFSIZ
+            f.seek(block * BUFSIZ, 2)
+            # read BUFFER
+            data.insert(0, f.read(BUFSIZ))
+        else:
+            # file too small, start from beginning
+            f.seek(0,0)
+            # only read what was not read
+            data.insert(0, f.read(bytes_i))
+        linesFound = data[0].count('\n')
+        size -= linesFound
+        bytes_i -= BUFSIZ
+        block -= 1
+    return ''.join(data).splitlines()[-window:]
