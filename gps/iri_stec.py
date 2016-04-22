35742739212c2472c8b387d3e17730f563fe3722
Mark D. Butala <butala@illinois.edu>
2016-04-22 16:51:17 -0500

    Now output full HDF5 record.

--- gps/iri_stec.py
+++ gps/iri_stec.py
@@ -3,14 +3,27 @@ import sys
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from itertools import repeat, izip
 from multiprocessing import Pool, cpu_count
-from collections import OrderedDict, defaultdict
-from cPickle import dump
+from collections import OrderedDict, namedtuple
+
+from tables import open_file, IsDescription, Time64Col, Float64Col
 
 from ..gpstk import PyPosition
 from ..util.path import SmartTempDir, replace_path
+from ..util.date import UNIX_EPOCH
 from ..iri.iri_stec import iri_stec
 from rinex import dump_preprocessed_rinex, read_rindump
 from observation import ObsMapFlatIterator
+
+
+class STecInfo(namedtuple('StecInfo',
+                          'stec az el satx saty satz')):
+    pass
+
+
+class STecMap(dict):
+    def __missing__(self, key):
+        self[key] = OrderedDict()
+        return self[key]
 
 
 def iri_stec_wrap(x):
@@ -20,7 +33,14 @@ def iri_stec_wrap(x):
     sat_pos = PyPosition(obs.satx,
                          obs.saty,
                          obs.satz)
-    return sat, dt, iri_stec(dt, stn_pos, sat_pos)
+    return (sat,
+            dt,
+            STecInfo(iri_stec(dt, stn_pos, sat_pos),
+                     obs.az,
+                     obs.el,
+                     obs.satx,
+                     obs.saty,
+                     obs.satz))
 
 
 def rinex_iri_stec(obs_fname,
@@ -44,10 +64,47 @@ def rinex_iri_stec(obs_fname,
         pool.close()
         pool.join()
 
-        stec_map = defaultdict(OrderedDict)
-        for sat, dt, stec in stec_output:
-            stec_map[sat][dt] = stec
+        stec_map = STecMap()
+        stec_map.xyz = obs_map.xyz
+        stec_map.llh = obs_map.llh
+        for sat, dt, stec_info in stec_output:
+            stec_map[sat][dt] = stec_info
     return stec_map
+
+
+class STecTable(IsDescription):
+    dt   = Time64Col()
+    stec = Float64Col()
+    az   = Float64Col()
+    el   = Float64Col()
+    satx = Float64Col()
+    saty = Float64Col()
+    satz = Float64Col()
+
+
+def dump_stec_map(h5_fname, stec_map):
+    """ ??? """
+    h5file = open_file(h5_fname, mode='w', title='IRI simulated slant TEC')
+    group = h5file.create_group('/', 'phase_arcs', 'Phase connected arcs')
+    if hasattr(stec_map, 'xyz'):
+        group._v_attrs.xyz = stec_map.xyz
+    if hasattr(stec_map, 'llh'):
+        group._v_attrs.llh = stec_map.llh
+    for sat in sorted(stec_map):
+        assert sat[0] == 'G'
+        table = h5file.create_table(group, sat, STecTable, 'GPS prn={} data'.format(sat[1:]))
+        row = table.row
+        for dt, stec_info in stec_map[sat].iteritems():
+            row['dt'] = (dt - UNIX_EPOCH).total_seconds()
+            row['stec'] = stec_info.stec
+            row['az'] = stec_info.az
+            row['el'] = stec_info.el
+            row['satx'] = stec_info.satx
+            row['saty'] = stec_info.saty
+            row['satz'] = stec_info.satz
+            row.append()
+        table.flush()
+    return h5_fname
 
 
 def main(argv=None):
@@ -56,9 +113,9 @@ def main(argv=None):
 
     parser = ArgumentParser('Compute IRI slant TEC along lines of sight found in given RINEX file.',
                             formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument('output_pkl_fname',
+    parser.add_argument('output_h5_fname',
                         type=str,
-                        help='output pickle file')
+                        help='output HDF5 file')
     parser.add_argument('rinex_obs_fname',
                         type=str,
                         help='input RINEX observation file')
@@ -88,8 +145,8 @@ def main(argv=None):
                               decimate=args.decimate,
                               processes=args.processes)
 
-    with open(args.output_pkl_fname, 'w') as fid:
-        dump(stec_map, fid, -1)
+    dump_stec_map(args.output_h5_fname,
+                  stec_map)
 
 
 if __name__ == '__main__':
