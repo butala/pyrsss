081d0ba80fb98b5a8ee7663cb589cbbf95edc103
Mark D. Butala <butala@illinois.edu>
2016-06-18 16:18:08 -0500

    Added IONEX override.

--- gps/process.py
+++ gps/process.py
@@ -15,7 +15,8 @@ def process(path,
             rinex_fnames,
             nav_fname,
             work_path=None,
-            discfix_args=[]):
+            discfix_args=[],
+            ionex_fname=None):
     """
     ???
     """
@@ -54,16 +55,20 @@ def process(path,
         ionex_map = {}
         for level_h5_i, rinex_fname in zip(level_h5, rinex_fnames):
             date = fname2date(rinex_fname)
-            if date not in ionex_map:
-                logger.info('fetching IONEX for {:%Y-%m-%d}'.format(date))
-                ionex_map[date] = fetch_sideshow_ionex(work_path, date)
+            if ionex_fname:
+                ionex_fname_date = ionex_fname
+            else:
+                if date not in ionex_map:
+                    logger.info('fetching IONEX for {:%Y-%m-%d}'.format(date))
+                    ionex_map[date] = fetch_sideshow_ionex(work_path, date)
+                ionex_fname_date = ionex_map[date]
             logger.info('calibrating {}'.format(level_h5_i))
             try:
                 calibrated_h5.append(
                     bias_process(replace_path(path,
                                               rinex_fname + '.h5'),
                                  level_h5_i,
-                                 ionex_map[date]))
+                                 ionex_fname_date))
             except Exception as e:
                 logger.warning('bias calibration step failed for {} ({}) --- '
                                'skipping'.format(level_h5_i, e))
@@ -111,13 +116,19 @@ def main(argv=None):
                         type=add_dashes,
                         default=[],
                         help='options to pass to the GPSTk discontinuity fixer (see help message for pyrsss.gps.phase_edit) --- do not include the dashes')
+    parser.add_argument('--ionex-fname',
+                        '-i',
+                        type=str,
+                        default=None,
+                        help='use the specified IONEX record for satellite biases and VTEC (if not specified, download automatically from JPL sideshow)')
     args = parser.parse_args(argv[1:])
 
     process(args.path,
             args.rinex_fnames,
             args.nav_fname,
             work_path=args.work_path,
-            discfix_args=args.discfix_options)
+            discfix_args=args.discfix_options,
+            ionex_fname=args.ionex_fname)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
