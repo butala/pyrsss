ef304a6faf0c89bc60afaecabee6d263ee3509c3
butala <butala@illinois.edu>
2016-09-13 10:31:46 -0500

    Added the add_colorbar function.

--- util/plot.py
+++ util/plot.py
@@ -1,6 +1,8 @@
 from __future__ import division
 
 import numpy as NP
+import pylab as PL
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
 def centers2edges(x):
@@ -12,3 +14,19 @@ def centers2edges(x):
     return NP.linspace(x[0] - delta / 2,
                        x[-1] + delta / 2,
                        len(x) + 1)
+
+
+def add_colorbar(ax, im, side='right', size='5%', pad=0.1):
+    """
+    Add colorbar to the axes *ax* with colors corresponding to the
+    color mappable object *im*. Place the colorbar at the *side* of
+    *ax* (options are `'right'`, `'left'`, `'top'`, or
+    `'bottom'`). The width (or height) of the colorbar is specified by
+    *size* and is relative to *ax*. Add space *pad* between *ax* and
+    the colorbar. Return the colorbar instance.
+
+    Reference: http://matplotlib.org/mpl_toolkits/axes_grid/users/overview.html
+    """
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes(side, size=size, pad=pad)
+    return PL.colorbar(im, cax=cax)
