fb49580ec0368dff8eb937bc048bbf9b25b83e0b
Mark D. Butala <butala@illinois.edu>
2017-02-03 13:11:50 -0600

    Added FIR filter response check function and feature to lp_fir_filter.
    The feature being the option to pass in the time coordinates and slice
    it in the same way as the given mode, i.e., full, same, or valid.

--- pyrsss/util/signal.py
+++ pyrsss/util/signal.py
@@ -2,8 +2,12 @@ from __future__ import division
 
 import math
 import logging
+from collections import OrderedDict
 
 import numpy as NP
+
+from ..util.signal import nextpow2, lp_fir_type
+from ..stats.stats import Stats
 
 logger = logging.getLogger('pyrsss.util.signal')
 
@@ -85,7 +89,7 @@ def lp_fir_type(h):
     assert False
 
 
-def lp_fir_filter(h, x, real=True, mode='same'):
+def lp_fir_filter(h, x, real=True, mode='same', index=None):
     """
     Apply a linear phase FIR filter with impulse response *h* to the
     signal *x* and return the output (with same length as *x*) after
@@ -98,6 +102,10 @@ def lp_fir_filter(h, x, real=True, mode='same'):
     1`). Finally if mode is 'valid', return only that portion for
     which *h* and *x* completely overlap (i.e., the portion where no 0
     boundary values are included).
+
+    If *index* is provided, return the slice of *index* corresponding
+    to *mode*. The purpose is to associate the correct time indices
+    with the filter output.
     """
     lp_type = lp_fir_type(h)
     if lp_type is None:
@@ -115,15 +123,90 @@ def lp_fir_filter(h, x, real=True, mode='same'):
         y = NP.real(y)
     M = len(h) - 1
     if mode == 'same':
-        return y[int(M/2):int(M/2) + len(x)]
+        y_out = y[int(M/2):int(M/2) + len(x)]
+        if index:
+            index_out = index[int(M/2):int(M/2) + len(x)]
     elif mode == 'valid':
         D_min = min(N, K)
         D_max = max(N, K)
-        return y[(D_min - 1):D_max]
+        y_out = y[(D_min - 1):D_max]
+        if index:
+            index_out = index[(D_min - 1):D_max]
     elif mode == 'full':
-        return y
+        y_out = y
+        index_out = index
     else:
         raise ValueError('unknown convolution mode {} (choices are same, valid, or full)')
+    if index:
+        return y_out, index_out
+    else:
+        return y_out
+
+
+def fir_response(h, bands, desired, Hz=1, names=None, verbose=True):
+    """
+    Report on the frequency response magnitude characteristics of the
+    filter with impulse response *h*. The list-like *bands* is twice
+    the length of *desired*, i.e., one filter desired magnitude per
+    two value frequency band specification, see
+    :func:`scipy.signal.remez`). The parameter *Hz* is the sample rate
+    and *names*, if given, associates a string name with each
+    band. Return a mapping from band identifiers to absolute desired
+    to filter magnitude per band :class:`Stats` and the median
+    value. If *vervose*, dump a report to stdout.
+    """
+    # check for argument consistency
+    bands = NP.array(bands)
+    if len(bands) % 2 != 0:
+        raise ValueError('# of band boundaries must be even')
+    if len(bands) / 2 != len(desired):
+        raise ValueError('# of band boundaries must equal to twice the length of desired (i.e., 2 boundaries per band and 1 desired amplitude per band)')
+    if any(bands < 0):
+        raise ValueError('no value of bands may be negative')
+    if any(bands > Hz / 2):
+        raise ValueError('no value of bands may be larger than the Nyquist rate (Hz / 2)')
+    if names:
+        if len(names) != len(desired):
+            raise ValueError('there should be an equal number of bands as names')
+    # compute filter frequency response
+    L = nextpow2(10 * len(h))
+    H = NP.fft.rfft(h, n=L)
+    f = NP.fft.rfftfreq(L, 1/Hz)
+    # gather stats per band
+    band_stats = OrderedDict()
+    medians = OrderedDict()
+    band_tuples = zip(bands[::2], bands[1::2])
+    for index, ((b1, b2), d) in enumerate(zip(band_tuples, desired)):
+        I = [i for  i, f_i in enumerate(f) if b1 <= f_i < b2]
+        diff = d - NP.abs([H[i] for i in I])
+        band_stats[index] = Stats(*NP.abs(diff)), NP.median(diff)
+        if names:
+            band_stats[names[index]] = band_stats[index]
+    if vervose:
+        # report on linear phase determined from symmetry of h
+        fir_type = lp_fir_type(h)
+        if fir_type:
+            print('FIR filter is linear phase type {}'.format(fir_type))
+        else:
+            print('FIR filter is NOT linear phase')
+        print('')
+        # output report
+        for index, d in enumerate(desired):
+            b1, b2 = band_tuples[index]
+            stats, medians = band_stats[index]
+            if names:
+                print('{} ({}) abs deviations from {}:'.format(names[index], index, d))
+            else:
+                print('band {} abs deviations from {}:'.format(index, d))
+            print('min = {:.3e}  (db={:f})'.format(stats.min,
+                                                   20 * math.log10(stats.min)))
+            print('med = {:.3e}'.format(medians))
+            if d == 1:
+                print('std = {:.3e}'.format(stats.sigma))
+            print('max = {:.3e} (db={:f}'.format(stats.max,
+                                                 20 * math.log10(stats.max)))
+            print('')
+    return band_stats
 
 
 if __name__ == '__main__':
