567302ca16783d610423f84b0070aa2cae50798a
Mark D. Butala <butala@illinois.edu>
2017-02-03 21:23:17 -0600

    Added tenth of arcminute conversion routines.
    These are required for declination_base in the USGS geomag algorithms.

--- pyrsss/util/angle.py
+++ pyrsss/util/angle.py
@@ -1,5 +1,23 @@
+import numpy as NP
+
+
 def convert_lon(lon):
     """
     Return 0 <= *lon* < 360 converted to -180 <= *lon < 180.
     """
     return lon - 360 if lon > 180 else lon
+
+
+def deg2tenths_of_arcminute(deg):
+    """
+    Return *deg* converted to tenths of arcminutes (i.e., arcminutes *
+    10).
+    """
+    return 10 * deg * 60
+
+
+def rad2tenths_of_arcminutes(rad):
+    """
+    Return *rad* convert to tenths of arcminutes.
+    """
+    return deg2tenths_of_minute(NP.degrees(rad))
