35f238e30af51d6adec0e18f916c73e93a9b666e
Mark D. Butala <butala@illinois.edu>
2017-02-04 00:45:16 -0600

    Added numerous comments, --he, and decbas search.

--- pyrsss/mag/iaga2hdf.py
+++ pyrsss/mag/iaga2hdf.py
@@ -19,9 +19,11 @@ logger = logging.getLogger('pyrsss.mag.iaga2hdf')
 
 def find_value(key, headers):
     """
-    ???
+    Search *headers* for *key* and return the common value shared
+    across each element of *headers* and raise an exception if
+    multiple values are encountered.
     """
-    values = set([x[key] for x in headers])
+    values = set([getattr(x, key, None) for x in headers if getattr(x, key, None) is not None])
     if len(values) == 0:
         raise KeyError('{} not found'.format(key))
     elif len(values) > 1:
@@ -35,13 +37,21 @@ def reduce_headers(headers,
                          'Geodetic_Latitude',
                          'Geodetic_Longitude',
                          'Elevation',
-                         'Reported']):
+                         'Reported',
+                         'Comment']):
+    """
+    Search *headers* for values associated with each of *keys* and
+    confirm those values are equal. Return a mapping between *keys*
+    and each unique value.
+    """
     return {k: find_value(k, headers) for k in keys}
 
 
 def fix_sign(x, N=360 * 60 * 10):
     """
-    ???
+    Convert negative tenths of arcminutes *x* to positive by checking
+    bounds and taking the modulus N (360 degrees * 60 minutes per
+    degree * 10 tenths per 1).
     """
     if x < 0:
         assert x > -N
@@ -55,9 +65,13 @@ def build_stream(header,
                  dec_tenths_arcminute,
                  network='NT',
                  location='R0',
-                 ne=False):
+                 default_elevation=0,
+                 he=False):
     """
-    ???
+    Build obspy :class:`Stream` from *header* information and the data
+    arranged in *data_maps*. Use *dec_tenths_arcminute* (local
+    magnetic declination in determining magnetic north and east or XYZ
+    in unite of tenths of arcminutes).
     """
     glon = header['Geodetic_Longitude']
     if glon < 0:
@@ -80,8 +94,8 @@ def build_stream(header,
     try:
         stream_header['elevation'] = header['elevation']
     except KeyError:
-        stream_header['elevation'] = '???'
-        logger.warning('elevation is unknown --- inserting ???')
+        stream_header['elevation'] = default_elevation
+        logger.warning('elevation is unknown --- inserting {}'.format(default_elevation))
     traces = []
     for channel in ['x', 'y', 'z', 'f']:
         vals = [getattr(x, channel) for dm in data_maps for x in dm.itervalues()]
@@ -96,10 +110,9 @@ def build_stream(header,
 
 def stream2dt(stream):
     """
-    ???
+    Verify time indices of all *stream* traces are equal and return a
+    list of sample of time suitable for a :class:`DataFrame` index.
     """
-    #[t.stats.starttime + x for t in stream.traces for x in t.times()]
-
     dt_match = None
     for t in stream.traces:
         if dt_match is None:
@@ -111,25 +124,47 @@ def stream2dt(stream):
     return dt_match
 
 
-def stream2df(geo, ne=False):
+def stream2df(geo, he=False):
     """
-    ???
+    Build and return :class:`DataFrame` from *geo* stream elements. By
+    default, these are traces x, y, z, and f. If *he*, also include
+    the mag north, h, and mag east, e, components.
     """
     if len(set(map(len, geo.traces))) != 1:
         raise ValueError('size mismatch detected (dt and all geo traces should be the same length)')
     data = {'B' + k: geo.select(channel=k).traces[0].data for k in ['x', 'y', 'z', 'f']}
-    if ne:
+    if he:
         for k in ['h', 'e']:
-            data['B' + k] = ne.select(channel=k).traces[0].data
+            data['B' + k] = he.select(channel=k).traces[0].data
     return PD.DataFrame(index=stream2dt(geo),
                         data=data)
 
 
+
+def find_decbas(header):
+    """
+    Search IAGA *header* Comment for DECBAS. Return either this
+    quantity (declination in tenths of arcmin) or `None` if not found.
+    """
+    if 'Comment' in header:
+        if 'DECBAS' in header['Comment']:
+            for line in header['Comment'].split('\n'):
+                if line.startswith('DECBAS'):
+                    _, decbas_str, _ = line.split(None, 2)
+                    return float(decbas_str)
+    return None
+
+
 def iaga2hdf(hdf_fname,
              iaga2002_fnames,
-             ne=False,
+             he=False,
              strict=False):
-    """ ??? """
+    """
+    Convert data found in IAGA 2002 files *iaga2002_fname* to an HDF
+    record. If *he*, store the h (mag north) and e (mag east)
+    components. If *strict*, use strict conformity checks when parsing
+    the IAGA 2002 records.
+    """
     headers = []
     data_maps = []
     for fname in iaga2002_fnames:
@@ -140,19 +175,23 @@ def iaga2hdf(hdf_fname,
     header = reduce_headers(headers)
     if header['Reported'] != 'XYZF':
         raise NotImplementedError('only XYZF implemented so far')
-    if ne and header['Reported'] == 'XYZF':
-        # transform to obs coodinates
-        date = data_maps[0].keys()[0]
-        point = Point(date,
-                      header['Geodetic_Latitude'],
-                      header['Geodetic_Longitude'],
-                      header['Elevation'])
-        point.run_igrf()
-        dec_deg = point.dec
-        dec_tenths_arcminute = fix_sign(deg2tenths_of_arcminute(dec_deg))
-    geo = build_stream(header, data_maps, dec_tenths_arcminute, ne=ne)
-    obs = get_obs_from_geo(geo) if ne else False
-    df = stream2df(geo, ne=obs)
+    if he and header['Reported'] == 'XYZF':
+        decbas = find_decbas(header)
+        if decbas:
+            dec_tenths_arcminute = decbas
+        else:
+            # transform to obs coordinates
+            date = data_maps[0].keys()[0]
+            point = Point(date,
+                          header['Geodetic_Latitude'],
+                          header['Geodetic_Longitude'],
+                          header['Elevation'])
+            point.run_igrf()
+            dec_deg = point.dec
+            dec_tenths_arcminute = fix_sign(deg2tenths_of_arcminute(dec_deg))
+    geo = build_stream(header, data_maps, dec_tenths_arcminute, he=he)
+    obs = get_obs_from_geo(geo) if he else False
+    df = stream2df(geo, he=obs)
     df.to_hdf(hdf_fname, 'B', mode='w')
     return hdf_fname
 
@@ -171,14 +210,14 @@ def main(argv=None):
                         metavar='iaga2002_fname',
                         nargs='*',
                         help='input IAGA2002 file (in time order)')
-    parser.add_argument('--ne',
+    parser.add_argument('--he',
                         action='store_true',
-                        help='include data in NE coordinate')
+                        help='include data in HE coordinate')
     args = parser.parse_args(argv[1:])
 
     iaga2hdf(args.hdf_fname,
              args.iaga2002_fnames,
-             ne=args.ne)
+             he=args.he)
 
 
 if __name__ == '__main__':
