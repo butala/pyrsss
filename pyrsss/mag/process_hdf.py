6a50762212a0a223c9c10503c8a0ba917ff4d8c2
Mark D. Butala <butala@illinois.edu>
2017-02-06 11:21:17 -0600

    Changed minute interval filter to Remez.
    Also added a bunch of documentation.

--- pyrsss/mag/process_hdf.py
+++ pyrsss/mag/process_hdf.py
@@ -16,8 +16,35 @@ from ..util.signal import lp_fir_filter
 logger = logging.getLogger('pyrsss.mag.process_hdf')
 
 
-def minute_interval_filter():
-    """ ??? """
+def minute_interval_filter_firwin():
+    """
+    Synthesize and return impulse response of minute interval filter
+    for analysis of magnetometer data. Design a filter using the
+    window method. The filter is designed to stop from 0 to 0.75 mHz
+    and pass from 1 mHz to Nyquist.
+
+    For reference: this is the filter used to process the data for the
+    Space Weather 2017 paper.
+
+    fs_1m = 1./60
+    bands_1m = [0, 1e-3 - 0.25e-3, 1e-3, fs_1m/2]
+    desired_1m = [0, 1]
+
+    _ = fir_response(h_1m, bands_1m, desired_1m, Hz=fs_1m)
+
+    FIR filter is linear phase type 1
+
+    band 0 abs deviations from 0:
+    min = 4.466e-05  (db=-87.000846)
+    med = -4.218e-03
+    max = 3.027e-02 (db=-30.379491
+
+    band 1 abs deviations from 1:
+    min = 4.412e-06  (db=-107.106525)
+    med = 2.398e-03
+    std = 4.059e-03
+    max = 2.982e-02 (db=-30.509428
+    """
     Ts = 60
     fs = 1. / Ts
     fn = fs / 2
@@ -34,11 +61,72 @@ def minute_interval_filter():
                                nyq=fn)
 
 
-def second_interval_filter():
+def minute_interval_filter(N_remez=201):
     """
-    DOCUMENT 4001 CHOICE!!!
+    Synthesize and return impulse response of minute interval filter
+    for analysis of magnetometer data. Design a length *N_remez*
+    min-max optimal filter. The filter is designed to stop from 0 to
+    0.75 mHz and pass from 1 mHz to Nyquist.
+
+    fs_1m = 1./60
+    bands_1m = [0, 1e-3 - 0.25e-3, 1e-3, fs_1m/2]
+    desired_1m = [0, 1]
+
+    _ = fir_response(h_1m, bands_1m, desired_1m, Hz=fs_1m)
+
+    FIR filter is linear phase type 1
+
+    band 0 abs deviations from 0:
+    min = 1.487e-05  (db=-96.553225)
+    med = -1.102e-03
+    max = 1.569e-03 (db=-56.089238
+
+    band 1 abs deviations from 1:
+    min = 1.018e-06  (db=-119.848775)
+    med = -1.066e-05
+    std = 4.820e-04
+    max = 1.571e-03 (db=-56.074809
     """
-    N_remez = 4001
+    return scipy.signal.remez(N_remez,
+                              [0, 0.75e-3, 1e-3, 1./60/2],
+                              [0, 1],
+                              [1, 1],
+                              Hz=1./60)
+
+
+def second_interval_filter(N_remez=4001):
+    """
+    Synthesize and return impulse response of second interval filter
+    for analysis of magnetometer data. Design a length *N_remez*
+    min-max optimal filter. The filter is designed to stop from 0 to
+    0.25 mHz, pass from 1 to 100 mHz, and stop from 101 mHz to
+    Nyquist.
+
+
+    fs_1m = 1./60
+    bands_1m = [0, 1e-3 - 0.25e-3, 1e-3, fs_1m/2]
+    desired_1m = [0, 1]
+
+    _ = fir_response(h_1m, bands_1m, desired_1m, Hz=fs_1m)
+
+    FIR filter is linear phase type 1
+
+    band 0 abs deviations from 0:
+    min = 6.872e-05  (db=-83.257739)
+    med = -2.271e-03
+    max = 4.902e-03 (db=-46.192275
+
+    band 1 abs deviations from 1:
+    min = 2.323e-07  (db=-132.678419)
+    med = -6.093e-07
+    std = 5.871e-04
+    max = 2.349e-03 (db=-52.584106
+
+    band 2 abs deviations from 0:
+    min = 1.430e-07  (db=-136.893586)
+    med = -1.353e-03
+    max = 2.853e-03 (db=-50.895031
+    """
     return scipy.signal.remez(N_remez,
                               [0, 0.25e-3, 1e-3, 100e-3, 101e-3, 0.5],
                               [0, 1, 0],
@@ -46,9 +134,10 @@ def second_interval_filter():
                               Hz=1)
 
 
-
 def consecutive_nans(x, y):
     """
+    Return the maximum number of nans encountered in the logical or of
+    the *x* and *y* time series.
     """
     nan = NP.isnan(x) | NP.isnan(y)
     lengths = []
@@ -68,6 +157,11 @@ def process_timeseries(dt,
                        c2='By',
                        remove_mean=True):
     """
+    Process surface magnetic field measurement time series with
+    indices *dt* and components *Bx* and *By*. Output a
+    :class:`DataFrame` with columns *c1* and *c2* associated with the
+    processed output time series. If *remove_mean*, remove the mean
+    from the output time series.
     """
     n = consecutive_nans(Bx, By)
     interval = (dt[1] - dt[0]).total_seconds()
@@ -101,7 +195,11 @@ def process(hdf_fname,
             he=False,
             remove_mean=True):
     """
-    ???
+    Process the magnetic field columns of *hdf_fname*, applying
+    pre-processing (nan interpolation) and a band-pass filter. Look
+    for input at *source_key* and store output under identifier
+    *key*. If *he*, process the H and E magnetic field components. If
+    *remove_mean*, remove the mean from each column.
     """
     logger.info('processing {}'.format(hdf_fname))
     df_raw, header = read_hdf(hdf_fname, source_key)
