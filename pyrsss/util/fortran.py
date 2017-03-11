34996aece61f4cf15ed7dbf1c357a6a2e54c4a49
Mark D. Butala <butala@illinois.edu>
2017-03-11 13:20:46 -0600

    Cleanup.

--- pyrsss/util/fortran.py
+++ pyrsss/util/fortran.py
@@ -1,5 +1,8 @@
 def descriptor2colspecs(descriptor_str):
     """
+    Convert fortran format string, e.g., `2I4,4I3,3I4,2I7,F6.2,I7,
+    8F8.2,4F8.1,F7.2,F9.0,F6.2,2F7.2,F6.1,6F8.2,7I6,F7.2,F5.1`, to the
+    list of fixed columns tuples expected by :func:`pandas.read_fwf`.
     """
     colspecs = []
     i = 0
@@ -11,17 +14,11 @@ def descriptor2colspecs(descriptor_str):
         else:
             tok_chop_left = tok
             tok_left = '1'
-        # print('----')
-        # print(tok)
-        # print(tok_chop_left)
-        # print(tok_left, '???')
         tok_type = tok_chop_left[0]
         assert tok_chop_left[1].isdigit()
         tok_right = tok_chop_left[1:]
-        # print(tok_right)
         multiplyer = int(tok_left)
         N = int(tok_right.split('.')[0])
-        #tok_len = multiplyer * N
         for n in range(multiplyer):
             colspecs.append((i, i + N))
             i += N
