4e0c2a8685175542b6c44582d429f2896de1b143
Mark D. Butala <butala@illinois.edu>
2017-09-06 11:37:38 -0500

    Revamp of the Kalman filter code.
    Now using numpy.matmul (saner treatment of matrix-vector
    multiplication) and updated docstring.

--- pyrsss/kalman/kalman_filter.py
+++ pyrsss/kalman/kalman_filter.py
@@ -1,40 +1,44 @@
 from __future__ import division
 
-from itertools import izip
+from itertools import izip, repeat
 
 import numpy as NP
 from scipy.linalg import cho_factor, cho_solve
 
 
-def is_col_vector(x):
-    """
-    ???
-    """
-    return x.ndim == 2 and x.shape[1] == 1
-
-
 def kalman_filter(y, H, R, F, Q, mu, PI, z=None):
     """
-    ???
+    Given the following sequences (one item of given dimension for
+    each time step):
+    - *y*: measurements (M)
+    - *H*: measurement operator (MxN)
+    - *R*: measurement noise covariance (MxM)
+    - *F*: time update operator (NxN)
+    - *Q*: time update noise covariance (NxN)
+    - *mu*: initial state (N)
+    - *PI*: initial state covariance (NxN)
+    - *z*: (optional) systematic time update input (N)
+
+    Return the tuple of lists of posterior state estimates and error
+    covariances.
     """
     x_hat = []
     P = []
-    assert is_col_vector(mu)
     x_hat_prior = mu
     P_prior = PI
-    for i, (y_i, H_i, R_i, F_i, Q_i) in enumerate(izip(y, H, R, F, Q)):
-        assert is_col_vector(y_i)
+    if z is None:
+        z = repeat(None)
+    for i, (y_i, H_i, R_i, F_i, Q_i, z_i) in enumerate(izip(y, H, R, F, Q, z)):
         # measurement update
-        A = cho_factor(NP.dot(H_i, NP.dot(P_prior, H_i.T)) + R_i)
-        B = cho_solve(A, NP.dot(H_i, P_prior))
-        b = cho_solve(A, y_i - NP.dot(H_i, x_hat_prior))
-        C = NP.dot(P_prior, H_i.T)
-        x_hat.append(x_hat_prior + NP.dot(C, b))
-        P.append(P_prior - NP.dot(C, B))
+        A = cho_factor(NP.matmul(H_i, NP.matmul(P_prior, H_i.T)) + R_i)
+        B = cho_solve(A, NP.matmul(H_i, P_prior))
+        b = cho_solve(A, y_i - NP.matmul(H_i, x_hat_prior))
+        C = NP.matmul(P_prior, H_i.T)
+        x_hat.append(x_hat_prior + NP.matmul(C, b))
+        P.append(P_prior - NP.matmul(C, B))
         # time update
-        x_hat_prior = NP.dot(F_i, x_hat[-1])
-        if z is not None:
-            assert is_col_vector(z[i])
-            x_hat_prior += z[i]
-        P_prior = NP.dot(F_i, NP.dot(P[-1], F_i.T)) + Q_i
+        x_hat_prior = NP.matmul(F_i, x_hat[-1])
+        if z_i is not None:
+            x_hat_prior += z_i
+        P_prior = NP.matmul(F_i, NP.matmul(P[-1], F_i.T)) + Q_i
     return x_hat, P
