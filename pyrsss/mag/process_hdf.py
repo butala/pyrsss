ff895c67f35f947c2155aa21cd9e80318d33f912
Mark D. Butala <butala@illinois.edu>
2017-11-27 10:07:04 +0800

    First cut at adding new processing pipeline.
    Note that these routines have yet to be thoroughly validated.

--- pyrsss/mag/process_hdf.py
+++ pyrsss/mag/process_hdf.py
@@ -4,7 +4,9 @@ import sys
 import logging
 import warnings
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from datetime import timedelta
 from itertools import groupby
+from collections import OrderedDict
 
 import numpy as NP
 import pandas as PD
@@ -12,11 +14,10 @@ import scipy.signal
 
 from iaga2hdf import read_hdf, write_hdf
 from ..util.nan import nan_interp
-from ..util.signal import lp_fir_filter
+from ..signal.filter import lp_fir_filter
 from ..stats.stats import despike
 
 logger = logging.getLogger('pyrsss.mag.process_hdf')
-
 
 
 """
@@ -255,6 +256,102 @@ def process(hdf_fname,
                                             despike_data=despike_data,
                                             remove_mean=remove_mean)
         df_filtered = df_filtered.join(df_he_filtered)
+    write_hdf(hdf_fname, df_filtered, key, header)
+    return hdf_fname
+
+
+def fill_nans(df, delta=None):
+    """
+    """
+    if not delta:
+        dt_diff = NP.diff(df.index.values)
+        delta_timedelta64 = min(dt_diff)
+        delta_seconds = delta_timedelta64 / NP.timedelta64(1, 's')
+        delta = timedelta(seconds=delta_seconds)
+    logger.info('Using delta = {} (s)'.format(delta.total_seconds()))
+    index_new = PD.date_range(start=df.index[0],
+                              end=df.index[-1],
+                              freq=delta)
+    missing = sorted(set(index_new) - set(df.index))
+    if missing:
+        logger.warning('Missing time indices (filled by NaNs):')
+        for x in missing:
+            logger.warning(x)
+    return df.reindex(index_new, copy=False), delta
+
+
+def nan_interpolate(df):
+    """
+
+    Reference:
+    https://stackoverflow.com/questions/29007830/identifying-consecutive-nans-with-pandas
+    """
+    sum_nan = df.isnull().sum()
+    df_null_int = df.isnull().astype(int)
+    for col in df.columns:
+        max_run = df[col].isnull().astype(int).groupby(df[col].notnull().astype(int).cumsum()).sum()
+        if sum_nan[col]:
+            # BELOW IS BROKEN!!!
+            pass
+            # logger.warning('column {} has {} NaNs ({} max consecutive run)'.format(col,
+                                                                                   # sum_nan[col],
+                                                                                   # max_run))
+    df.interpolate(inplace=True)
+    return df
+
+
+def process_df(df,
+               delta=None,
+               despike_data=True,
+               subtract_median=True):
+    """
+    """
+    if despike_data:
+        logger.info('despike')
+        df = despike(df)
+    logger.info('Fill gaps')
+    df, delta = fill_nans(df, delta=delta)
+    logger.info('Gap interpolation')
+    df = nan_interpolate(df)
+    # apply 1 - 100 mHz bandpass filter
+    interval = delta.total_seconds()
+    if interval == 1.0:
+        h = second_interval_filter()
+    elif interval == 60.0:
+        h = minute_interval_filter()
+    else:
+        raise ValueError('1 to 100 mHz filter not yet synthesized for {} s interval data'.format(interval))
+    data = OrderedDict()
+    dt = df.index.to_pydatetime()
+    for i, col in enumerate(df.columns):
+        logger.info('Band-pass filter {}'.format(col))
+        if i == 0:
+            col_filtered, dt_filtered = lp_fir_filter(h, df[col].values, mode='valid', index=dt)
+        else:
+            col_filtered = lp_fir_filter(h, df[col].values, mode='valid')
+        data[col] = col_filtered
+    df_filtered = PD.DataFrame(index=dt_filtered,
+                               data=data)
+    # remove median
+    if subtract_median:
+        logger.info('Subtract median')
+        df_filtered = df_filtered.sub(df.median(axis=1), axis=0)
+    return df_filtered
+
+
+def process_new(hdf_fname,
+                source_key='B_raw',
+                key='B',
+                despike_data=True,
+                subtract_median=True):
+    """
+    """
+    logger.info('processing {}'.format(hdf_fname))
+    df_raw, header = read_hdf(hdf_fname, source_key)
+    df = df_raw[['B_X', 'B_Y']] * 1e-9
+    df_filtered = process_df(df,
+                             despike_data=despike_data,
+                             subtract_median=subtract_median)
     write_hdf(hdf_fname, df_filtered, key, header)
     return hdf_fname
 
