95a33e8ccdcc098986faf17c195837feff531dd4
butala <mark.d.butala@gmail.com>
2019-04-25 15:42:55 +0800

    Now based off of numpy routine

--- pyrsss/signal/util.py
+++ pyrsss/signal/util.py
@@ -5,7 +5,7 @@ def zero_pad(x, n):
     """
     Return *x* zero padded to the dimensions specified in *n*.
     """
-    y = NP.zeros(n)
-    for index in NP.ndindex(x.shape):
-        y[index] = x[index]
-    return y
+    assert len(n) == x.ndim
+    return NP.pad(x,
+                  [(0, n_i - s_i) for n_i, s_i in zip(n, x.shape)],
+                  'constant')
