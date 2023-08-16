0fab258b250662c7be216e37b6fabab5366f0a9b
Mark D. Butala <butala@shaolin-2.local>
2023-08-16 15:56:21 +0100

    Many small updates, e.g., NP -> np and use of filters.

--- pyrsss/mag/process_hdf.py
+++ pyrsss/mag/process_hdf.py
@@ -6,11 +6,12 @@ from datetime import timedelta
 from itertools import groupby
 from collections import OrderedDict
 
-import numpy as NP
-import pandas as PD
+import numpy as np
+import pandas as pd
 import scipy.signal
 
 from .iaga2hdf import read_hdf, write_hdf
+from .filters import minute_interval_filter, second_interval_filter
 from ..util.nan import nan_interp
 from ..signal.lfilter import lp_fir_filter
 from ..stats.stats import despike
@@ -29,136 +30,12 @@ The following describes the XYZ convention
 - Z is the downward component of the geomagnetic field, same as before.
 """
 
-
-def minute_interval_filter_firwin():
-    """
-    Synthesize and return impulse response of minute interval filter
-    for analysis of magnetometer data. Design a filter using the
-    window method. The filter is designed to stop from 0 to 0.75 mHz
-    and pass from 1 mHz to Nyquist.
-
-    For reference: this is the filter used to process the data for the
-    Space Weather 2017 paper.
-
-    fs_1m = 1./60
-    bands_1m = [0, 1e-3 - 0.25e-3, 1e-3, fs_1m/2]
-    desired_1m = [0, 1]
-
-    _ = fir_response(h_1m, bands_1m, desired_1m, Hz=fs_1m)
-
-    FIR filter is linear phase type 1
-
-    band 0 abs deviations from 0:
-    min = 4.466e-05  (db=-87.000846)
-    med = -4.218e-03
-    max = 3.027e-02 (db=-30.379491
-
-    band 1 abs deviations from 1:
-    min = 4.412e-06  (db=-107.106525)
-    med = 2.398e-03
-    std = 4.059e-03
-    max = 2.982e-02 (db=-30.509428
-    """
-    Ts = 60
-    fs = 1. / Ts
-    fn = fs / 2
-    width = 0.25e-3
-    cutoff = 1e-3
-    ripple = 30
-    numtaps, beta = scipy.signal.kaiserord(ripple, width / fn)
-    if numtaps % 2 == 0:
-        numtaps += 1
-    return scipy.signal.firwin(numtaps,
-                               cutoff - width/2,
-                               width=width,
-                               pass_zero=False,
-                               nyq=fn)
-
-
-def minute_interval_filter(N_remez=201):
-    """
-    Synthesize and return impulse response of minute interval filter
-    for analysis of magnetometer data. Design a length *N_remez*
-    min-max optimal filter. The filter is designed to stop from 0 to
-    0.75 mHz and pass from 1 mHz to Nyquist.
-
-    fs_1m = 1./60
-    bands_1m = [0, 1e-3 - 0.25e-3, 1e-3, fs_1m/2]
-    desired_1m = [0, 1]
-
-    _ = fir_response(h_1m, bands_1m, desired_1m, Hz=fs_1m)
-
-    FIR filter is linear phase type 1
-
-    band 0 abs deviations from 0:
-    min = 1.487e-05  (db=-96.553225)
-    med = -1.102e-03
-    max = 1.569e-03 (db=-56.089238
-
-    band 1 abs deviations from 1:
-    min = 1.018e-06  (db=-119.848775)
-    med = -1.066e-05
-    std = 4.820e-04
-    max = 1.571e-03 (db=-56.074809
-    """
-    return scipy.signal.remez(N_remez,
-                              [0, 0.75e-3, 1e-3, 1./60/2],
-                              [0, 1],
-                              [1, 1],
-                              Hz=1./60)
-
-
-def second_interval_filter(N_remez=4001):
-    """
-    Synthesize and return impulse response of second interval filter
-    for analysis of magnetometer data. Design a length *N_remez*
-    min-max optimal filter. The filter is designed to stop from 0 to
-    0.25 mHz, pass from 1 to 100 mHz, and stop from 101 mHz to
-    Nyquist.
-
-
-    fs_1m = 1.
-    bands_1s = [0, 1e-3 - 0.25e-3,
-                1e-3, 100e-3,
-                101e-3, fs_1s/2]
-    desired_1s = [0, 1, 0]
-
-    _ = fir_response(h_1m, bands_1m, desired_1m, Hz=fs_1m)
-
-    FIR filter is linear phase type 1
-
-    band 0: 0 Hz -- 750 \muHz
-    abs deviations from 0 statistics:
-    min = 6.872e-05  (db=-83.257739)
-    med = -4.906e-02
-    max = 7.486e-01 (db=-2.514808)
-
-    band 1: 1 mHz -- 100 mHz
-    abs deviations from 1 statistics:
-    min = 2.323e-07  (db=-132.678419)
-    med = -6.093e-07
-    std = 5.871e-04
-    max = 2.349e-03 (db=-52.584106)
-
-    band 2: 101 mHz -- 500 mHz
-    abs deviations from 0 statistics:
-    min = 1.430e-07  (db=-136.893586)
-    med = -1.353e-03
-    max = 2.853e-03 (db=-50.895031)
-    """
-    return scipy.signal.remez(N_remez,
-                              [0, 0.25e-3, 1e-3, 100e-3, 101e-3, 0.5],
-                              [0, 1, 0],
-                              [1, 1, 1],
-                              Hz=1)
-
-
 def consecutive_nans(x, y):
     """
     Return the maximum number of nans encountered in the logical or of
     the *x* and *y* time series.
     """
-    nan = NP.isnan(x) | NP.isnan(y)
+    nan = np.isnan(x) | np.isnan(y)
     lengths = []
     for key, group in groupby(nan):
         if key == True:
@@ -194,7 +71,7 @@ def process_timeseries(dt,
     By = nan_interp(By)
     if despike_data:
         # remove outliers
-        df = PD.DataFrame(index=dt,
+        df = pd.DataFrame(index=dt,
                           data={'Bx': Bx,
                                 'By': By})
         df = despike(df)
@@ -212,10 +89,10 @@ def process_timeseries(dt,
     By_filtered = lp_fir_filter(h, By, mode='valid')
     # remove mean
     if remove_mean:
-        Bx_filtered -= NP.mean(Bx_filtered)
-        By_filtered -= NP.mean(By_filtered)
+        Bx_filtered -= np.mean(Bx_filtered)
+        By_filtered -= np.mean(By_filtered)
     # build DataFrame and store to disk
-    return PD.DataFrame(index=dt_filtered,
+    return pd.DataFrame(index=dt_filtered,
                         data={c1: Bx_filtered,
                               c2: By_filtered})
 
@@ -262,12 +139,12 @@ def fill_nans(df, delta=None):
     """
     """
     if not delta:
-        dt_diff = NP.diff(df.index.values)
+        dt_diff = np.diff(df.index.values)
         delta_timedelta64 = min(dt_diff)
-        delta_seconds = delta_timedelta64 / NP.timedelta64(1, 's')
+        delta_seconds = delta_timedelta64 / np.timedelta64(1, 's')
         delta = timedelta(seconds=delta_seconds)
     logger.info('using delta = {} (s)'.format(delta.total_seconds()))
-    index_new = PD.date_range(start=df.index[0],
+    index_new = pd.date_range(start=df.index[0],
                               end=df.index[-1],
                               freq=delta)
     missing = sorted(set(index_new) - set(df.index))
@@ -329,7 +206,7 @@ def process_df(df,
         else:
             col_filtered = lp_fir_filter(h, df[col].values, mode='valid')
         data[col] = col_filtered
-    df_filtered = PD.DataFrame(index=dt_filtered,
+    df_filtered = pd.DataFrame(index=dt_filtered,
                                data=data)
     # remove median
     if subtract_median:
