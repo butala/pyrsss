bea673639577147f811e43fbc73aa9d5528982c2
Mark D. Butala <butala@shaolin-2.local>
2023-08-16 13:43:26 +0100

    Small updates, e.g., NP -> np.

--- pyrsss/signal/convmtx.py
+++ pyrsss/signal/convmtx.py
@@ -1,4 +1,5 @@
-import numpy as NP
+import numpy as np
+import scipy as sp
 import scipy.linalg
 import scipy.signal
 import scipy.sparse as sparse
@@ -26,16 +27,16 @@ class Convmtx(sparse.coo_matrix):
                 return sparse.coo_matrix((k[-1], n[-1]))
             else:
                 c = h
-                r = NP.array([c[0]] + [0]*(n[-1]-1))
+                r = np.array([c[0]] + [0]*(n[-1]-1))
                 return sparse.coo_matrix(scipy.linalg.toeplitz(c, r))
 
         def toeplitz_mapper_valid(h):
             if (h == 0).all():
                 return sparse.coo_matrix((k[-1], n[-1]))
             else:
-                r = NP.zeros(n[-1])
+                r = np.zeros(n[-1])
                 r[:len(h)] = h
-                c = NP.zeros(k[-1])
+                c = np.zeros(k[-1])
                 c[0] = r[0]
                 return sparse.coo_matrix(scipy.linalg.toeplitz(c, r))
 
@@ -44,7 +45,7 @@ class Convmtx(sparse.coo_matrix):
                 return sparse.coo_matrix((k[-1], n[-1]))
             else:
                 c = h
-                r = NP.zeros(n[-1])
+                r = np.zeros(n[-1])
                 r[0] = c[0]
                 r[1:] = h[:0:-1]
                 return sparse.coo_matrix(scipy.linalg.toeplitz(c, r))
@@ -78,27 +79,27 @@ class Convmtx(sparse.coo_matrix):
         m = H.shape
 
         if mode == 'full':
-            k = tuple(NP.array(n) + NP.array(m) - 1)
+            k = tuple(np.array(n) + np.array(m) - 1)
             toeplitz_mapper = toeplitz_mapper_full
             block_mapper = block_mapper_full
 
             H_zp = zero_pad(H, k)
-            c_list = NP.split(H_zp.flatten(), NP.prod(k[:-1]))
+            c_list = np.split(H_zp.flatten(), np.prod(k[:-1]))
         elif mode == 'valid':
-            k = tuple(NP.array(n) - NP.array(m) + 1)
+            k = tuple(np.array(n) - np.array(m) + 1)
             toeplitz_mapper = toeplitz_mapper_valid
             block_mapper = block_mapper_valid
 
             H_zp = zero_pad(H[...,::-1], n)
-            c_list = NP.split(H_zp.flatten(), NP.prod(n[:-1]))
+            c_list = np.split(H_zp.flatten(), np.prod(n[:-1]))
         elif mode == 'circ':
-            assert((NP.array(m) <= NP.array(n)).all())
+            assert (np.array(m) <= np.array(n)).all()
             k = n
             toeplitz_mapper = toeplitz_mapper_circ
             block_mapper = block_mapper_circ
 
             H_zp = zero_pad(H, k)
-            c_list = NP.split(H_zp.flatten(), NP.prod(k[:-1]))
+            c_list = np.split(H_zp.flatten(), np.prod(k[:-1]))
         else:
             raise ValueError('Unknown mode {0}'.format(mode))
 
@@ -106,9 +107,9 @@ class Convmtx(sparse.coo_matrix):
 
         for n_i, k_i in zip(n[-2::-1], k[-2::-1]):
             if mode == 'full' or mode == 'circ':
-                blocks = [block_mapper(n_i, k_i, x) for x in NP.split(NP.array(blocks), len(blocks)/k_i)]
+                blocks = [block_mapper(n_i, k_i, x) for x in np.split(np.array(blocks), len(blocks)/k_i)]
             elif mode =='valid':
-                blocks = [block_mapper(n_i, k_i, x) for x in NP.split(NP.array(blocks), len(blocks)/n_i)]
+                blocks = [block_mapper(n_i, k_i, x) for x in np.split(np.array(blocks), len(blocks)/n_i)]
             else:
                 raise ValueError('Unknown mode {0}'.format(mode))
 
@@ -122,8 +123,8 @@ def ndcircconv(x, h):
     """
     n = x.shape
     m = h.shape
-    k = NP.array(n) + NP.array(m) - 1
-    return NP.real(NP.fft.ifftn(NP.fft.fftn(h, s=k) * NP.fft.fftn(x, s=k))).flat
+    k = np.array(n) + np.array(m) - 1
+    return np.real(sp.fft.ifftn(sp.fft.fftn(h, s=k) * sp.fft.fftn(x, s=k))).flat
 
 
 def random_validation(N,
@@ -139,61 +140,61 @@ def random_validation(N,
     """
     print('Testing circ mode')
     for i in range(1, N + 1):
-        n_dim = NP.random.random_integers(1, n_dim_max)
-        n = NP.random.random_integers(1, n_max, n_dim)
-        m = NP.random.random_integers(1, m_max, n_dim)
-        x = NP.random.randn(*n)
-        h = NP.arange(NP.prod(m)) + 1
+        n_dim = np.random.random_integers(1, n_dim_max)
+        n = np.random.random_integers(1, n_max, n_dim)
+        m = np.random.random_integers(1, m_max, n_dim)
+        x = np.random.randn(*n)
+        h = np.arange(np.prod(m)) + 1
         h.shape = m
-        k = NP.array(n) + NP.array(m) - 1
+        k = np.array(n) + np.array(m) - 1
         print('{} of {} (n={} k={} mode=circ)'.format(i, N, n, k))
         H = Convmtx(k, h, mode='circ')
         y_true = ndcircconv(x, h)
         x_zp = zero_pad(x, k)
         y_mtx = H * x_zp.flat
-        assert(NP.allclose(y_true, y_mtx))
+        assert np.allclose(y_true, y_mtx)
     print('')
     print('Testing full and valid modes')
     mode_list = ['full', 'valid']
     for i in range(1, N + 1):
-        n_dim = NP.random.random_integers(1, n_dim_max)
-        n = NP.random.random_integers(1, n_max, n_dim)
-        m = NP.random.random_integers(1, m_max, n_dim)
+        n_dim = np.random.random_integers(1, n_dim_max)
+        n = np.random.random_integers(1, n_max, n_dim)
+        m = np.random.random_integers(1, m_max, n_dim)
 
-        mode = mode_list[NP.random.random_integers(0, 1)]
+        mode = mode_list[np.random.random_integers(0, 1)]
         if mode == 'full':
-            k = tuple(NP.array(n) + NP.array(m) - 1)
+            k = tuple(np.array(n) + np.array(m) - 1)
         elif mode == 'valid':
-            k = tuple(NP.array(n) - NP.array(m) + 1)
+            k = tuple(np.array(n) - np.array(m) + 1)
         else:
-            assert(False)
+            assert False
 
-        x = NP.random.randn(*n)
-        h = NP.arange(NP.prod(m)) + 1
+        x = np.random.randn(*n)
+        h = np.arange(np.prod(m)) + 1
         h.shape = m
 
-        if (NP.array(k) <= 0).any():
-            assert(mode == 'valid')
+        if (np.array(k) <= 0).any():
+            assert mode == 'valid'
             n, m = m, n
             x, h = h, x
-            k = tuple(NP.array(n) - NP.array(m) + 1)
+            k = tuple(np.array(n) - np.array(m) + 1)
 
         print('{} of {} (n={} k={} mode={})'.format(i, N, n, k, mode))
 
-        if (NP.array(k) > 0).all():
+        if (np.array(k) > 0).all():
             H = Convmtx(n, h, mode=mode)
             y_true = scipy.signal.convolve(x, h, mode=mode)
             y_mtx = H * x.flat
             y_mtx.shape = k
 
-            assert(NP.allclose(y_true, y_mtx))
+            assert np.allclose(y_true, y_mtx)
         else:
             try:
                 y_true = scipy.signal.convolve(x, h, mode=mode)
             except ValueError:
                 pass
             else:
-                assert(NP.prod(y_true.shape) == 0)
+                assert np.prod(y_true.shape) == 0
     return True
 
 
