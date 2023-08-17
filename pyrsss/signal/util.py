4bca5c3b705ab04e463d09e04c748bcf920c6c6c
Mark D. Butala <butala@shaolin-2.local>
2023-08-17 16:44:16 +0100

    Change: NP -> np

--- pyrsss/signal/util.py
+++ pyrsss/signal/util.py
@@ -1,4 +1,4 @@
-import numpy as NP
+import numpy as np
 
 
 def zero_pad(x, n):
@@ -6,6 +6,6 @@ def zero_pad(x, n):
     Return *x* zero padded to the dimensions specified in *n*.
     """
     assert len(n) == x.ndim
-    return NP.pad(x,
+    return np.pad(x,
                   [(0, n_i - s_i) for n_i, s_i in zip(n, x.shape)],
                   'constant')
